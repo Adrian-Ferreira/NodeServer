558faed4db8b4a3bc550dc273caf82a6ba4944eb
Mayara <adrian.zoeferreira@gmail.com>
2019-12-10 14:48:02 -0300

    NodeSever

--- Xablau/helloworld.js
+++
@@ -1,5 +0,0 @@
-function mostra(nome) {
-    return "Hllo " + nome;
-}
-var pessoa = "Isidro";
-console.log(mostra(pessoa));
--- Xablau/helloworld.ts
+++
@@ -1,6 +0,0 @@
-function mostra(nome:string): string{
-    return "Hllo "+nome;
-}
-var pessoa:string = "Isidro";
-
-console.log(mostra(pessoa));
